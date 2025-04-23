01b566db2d9c7f778b34848f148cc134832aa092
Maxime Moreillon <m.moreillon@gmail.com>
2025-04-23 15:42:33 +0900

    prompt

--- client.ts
+++ client.ts
@@ -6,19 +6,22 @@ const url = "http://127.0.0.1:3000/mcp";
 const baseUrl = new URL(url);
 
 async function main() {
-  try {
-    const client = new Client({
-      name: "streamable-http-client",
-      version: "1.0.0",
-    });
-    const transport = new StreamableHTTPClientTransport(new URL(baseUrl));
-    await client.connect(transport);
-    console.log("Connected using Streamable HTTP transport");
-  } catch (error) {
-    // If that fails with a 4xx error, try the older SSE transport
-    console.log(error);
-    console.log("Streamable HTTP connection failed");
-  }
+  const client = new Client({
+    name: "streamable-http-client",
+    version: "1.0.0",
+  });
+  const transport = new StreamableHTTPClientTransport(new URL(baseUrl));
+  await client.connect(transport);
+  console.log("Connected using Streamable HTTP transport");
+
+  const prompt = await client.getPrompt({
+    name: "echo",
+    arguments: {
+      message: "This is my message",
+    },
+  });
+
+  console.log(prompt);
 }
 
 main();
